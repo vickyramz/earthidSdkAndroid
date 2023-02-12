c2f9d12be79d96eceaab507a8872d29732bd222e
Vigneshwar [C] Ramasamy <ramlvign@amazon.com>
2023-02-12 21:11:20 +0530

    v1.4

--- app/src/main/java/com/example/androidsdknew/MainActivity.java
+++ app/src/main/java/com/example/androidsdknew/MainActivity.java
@@ -2,16 +2,26 @@ package com.example.androidsdknew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.earthidlibservice.EarthidSdk;
 
 public class MainActivity extends AppCompatActivity {
-
+    final Activity activity = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button myButton = (Button) findViewById(R.id.my_button);
+        myButton.setOnClickListener(view -> {
+            EarthidSdk a =new EarthidSdk();
+          Intent i = a.createSdk(activity,"https://alchemy.veriff.com/v/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uX2lkIjoiNzU5YjA2NTEtMjZhNC00YzgxLTkxMTMtNjBjNWM0ZjU0ZTg5IiwiaWlkIjoiZTliNWQ2YmEtMWU1Yy00MTU3LWIzMzQtNWExN2FiMDY1ZTJhIiwiaWF0IjoxNjc2MjE1NzM2fQ.Reuu6blImhmDIN8VancMZYU-G906TzuGue7noB0OR54");
+          startActivityForResult(i,1);
+        });
 
     }
 }
--- earthidLibService/src/main/java/com/example/earthidlibservice/EarthidSdk.java
+++ earthidLibService/src/main/java/com/example/earthidlibservice/EarthidSdk.java
@@ -4,11 +4,22 @@ package com.example.earthidlibservice;
 import android.app.Activity;
 import android.content.Intent;
 
+import com.veriff.Branding;
+import com.veriff.Configuration;
 import com.veriff.Sdk;
 
 public class EarthidSdk {
     public Intent createSdk(Activity activity,String sessionUrl){
-        Intent intent = Sdk.createLaunchIntent(activity, sessionUrl);
+        Branding branding = new Branding.Builder()
+                .logo(R.drawable.earthidlogo_round)
+                // Button corner radius, in `dp`
+                .buttonRadius(48f)
+                .build();
+
+        Configuration configuration = new Configuration.Builder()
+                .branding(branding)
+                .build();
+        Intent intent = Sdk.createLaunchIntent(activity, sessionUrl,configuration);
         return  intent;
     }
 
